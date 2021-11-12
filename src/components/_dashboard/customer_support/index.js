8af0517f4977d53bb574402d68eebeca51769dbe
Vietnguyencong <vietnguyencong99@gmail.com>
2021-11-12 13:29:22 -0600

    yes commit about the message

---
+++ src/components/_dashboard/customer_support/index.js
@@ -0,0 +1,9 @@
+// export { default as ProductFilterSidebar } from './ProductFilterSidebar';
+// export { default as ProductCartWidget } from './ProductCartWidget';
+// export { default as ProductCard } from './ProductCard';
+// export { default as ProductList } from './ProductList';
+// export { default as ProductSort } from './ProductSort';
+export { default as Tasktbl } from './Task_tbl';
+export { default as Tickettbl } from './Ticket_tbl';
+export { default as Emailtbl } from './Email_tbl';
+export { default as Calltbl } from './Call_tbl';
--- src/pages/Customer_support.js
+++ src/pages/Customer_support.js
@@ -1,11 +1,27 @@
 import { Container, Grid } from '@mui/material';
 import React from 'react';
-
+import { Tasktbl, Tickettbl, Emailtbl, Calltbl } from 'src/components/_dashboard/customer_support';
 export default function Customer_support() {
   return (
-    <Container>
+    <Container maxWidth="xlo">
       <h2>Customer support</h2>
-      <Grid container spacing={3}></Grid>
+      <Grid container spacing={3}>
+        <Grid item xs={12} md={6} lg={6}>
+          <Tasktbl />
+        </Grid>
+        <Grid item xs={12} md={6} lg={6}>
+          <Tickettbl />
+        </Grid>
+      </Grid>
+      <br />
+      <Grid container spacing={3}>
+        <Grid item xs={12} md={6} lg={6}>
+          <Emailtbl />
+        </Grid>
+        <Grid item xs={12} md={6} lg={6}>
+          <Calltbl />
+        </Grid>
+      </Grid>
     </Container>
   );
 }
